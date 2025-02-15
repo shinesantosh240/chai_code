85267fb1ee90ebfad2bc2e7ebe7e086cba252567
shinesantosh240 <shinesantosh240@gmail.com>
2025-02-15 20:12:28 +0000

    conversion.js

--- .devcontainer/Basics/03_conversion.js
+++ .devcontainer/Basics/03_conversion.js
@@ -13,12 +13,36 @@
 // "33abc" =>Nan
 //true => 1; false =>0
 
-let isLoggedIn = true
-let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(isLoggedIn);
-// o => false 1 => true
-//"" => false; "Test"=> true
-let somenumber =null
-let stringNumber = String(somenumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// let isLoggedIn = true
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(isLoggedIn);
+// // o => false 1 => true
+// //"" => false; "Test"=> true
+// let somenumber =null
+// let stringNumber = String(somenumber)
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+// let str1 ='Hello'
+// let str2 = ' Rishi'
+// let str3 =str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log(1+2+"2")
+// console.log(true)
+// console.log(false)
+// console.log(-true)
+// console.log(+true)
+// console.log(-false)
+// console.log(+false)
+// console.log(+"")
+let x = 3;
+const y = x++;
+console.log(x,y);
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = ++x2;
+console.log(x2,y2);
