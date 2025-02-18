b36abb157de6dc4fad0ceb54f0c6b39b42295039
shinesantosh240 <shinesantosh240@gmail.com>
2025-02-18 18:09:48 +0000

    08_practise

---
+++ .devcontainer/Basics/07_nums_maths.js
@@ -0,0 +1,9 @@
+const score =400
+
+console.log(score)
+
+const balance = new Number(400)
+
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(2))
---
+++ .devcontainer/Basics/08_practise.js
@@ -0,0 +1,121 @@
+
+
+// let userOne ={
+//     email :"userone@gmail.com",
+//     upi :'user1@upi',
+//     userName:'userName'
+
+// }
+
+// let userTwo = userOne;
+// userTwo.email ='userTwo@gmail.com'
+
+// console.log(userOne.email)
+// console.log(userTwo.email)
+
+// string
+
+// const name ="Hitesh"
+// const repocount =50
+
+// console.log(`My name is ${name} and my repo count is ${repocount}`)
+
+
+// const gameName = new String("Hitesthc")
+
+// console.log(gameName)
+// console.log(gameName[0])
+
+// console.log(gameName.length)
+
+// console.log(gameName.charAt(2))
+
+// console.log(gameName.indexOf('t'))
+
+// console.log(gameName.substring(0,6))
+
+// console.log(gameName.slice(-4,8))
+
+// console.log('Hiteshhc'.slice(-4,8))
+
+// console.log('      Hites    hhc     '.trim())
+
+// const url ="https://google.com/20%/abc"
+// console.log(url.replace('20%/abc','.in'))
+
+// let text = "apple,banana;cherry|date";
+// let fruitsArray = text.split(/[]/);
+// console.log(fruitsArray);
+
+// let text = "Hello world, hello!";
+// let newText = text.replace(/hello/i, "Hi");
+// console.log(newText);
+
+// let texta = "Hello world!";
+// let bigText = texta.big();
+// console.log(bigTexta);
+
+// let num = 123.456789;
+// let fixedNum = num.toFixed(2);
+// console.log(fixedNum,typeof(fixedNum)); // Outputs: "123.46"
+
+// let number =1000000000
+// console.log(number.toLocaleString())
+
+// let maxValue = Number.MAX_VALUE;
+// console.log(maxValue.toLocaleString()); // Outputs: 1.7976931348623157e+308
+
+// let minValue = Number.MIN_VALUE;
+// console.log(minValue.toLocaleString());
+
+// let minValue = Number.MIN_VALUE;
+// console.log(minValue); // Outputs: 5e-324
+
+
+//++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// //absolute of any number - returns positive value
+// console.log(Math.abs(-4))
+
+// //math.round() -> it will round of the number 
+
+// console.log(Math.round(4.5))
+
+// //Math.ceiling() -> it will round to next number even for small change
+
+// console.log(Math.ceil(4.1))
+
+// //Math.floor() -> it will round lower number of the decimal value
+
+// console.log(Math.floor(2.3339))
+
+//console.log(Math.random())
+
+
+// console.log((Math.random() * 10 )+ 1);
+
+// console.log((Math.random() * 10 )+ 1); // Outputs a random number between 1 and 11
+
+// const max = 20;
+// const min =20;
+
+// //console.log(Math.floor(Math.random())*(max-min+1)) + min)
+
+
+//console.log(Math.floor(Math.random()*10)+1)
+
+//console.log((Math.floor(Math.random()*10)+1));
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const max =20;
+const min =10;
+
+console.log(Math.floor(Math.random()*(max-min))+min)
+
+
+
+
+
+
+
