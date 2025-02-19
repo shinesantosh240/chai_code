e3269c815e2b251d244687237f521ff94e1a6264
shinesantosh240 <shinesantosh240@gmail.com>
2025-02-19 03:45:36 +0000

    Array

---
+++ .devcontainer/Basics/10_Array.js
@@ -0,0 +1,43 @@
+
+//Array is a collection of element in a single variable
+// Array are resizable can contain mix element
+//Arrays are not associative
+
+let Num = [1,2,4,5]
+
+//let Heros = new Array("Shaktiman","Doga","Dhruv","Phantom")
+
+// Heros.push("JuniorG")
+// Num.push(0,7)
+
+// console.log(Num[0])
+// console.log(Num)
+
+// Num.unshift(9);
+// //console.log(Num)
+
+// Num.shift()
+
+//console.log(Num)
+// console.log(Heros)
+
+
+// //console.log(Num.includes(10))
+
+// console.log(Num.indexOf(3))
+
+// const newArr = Num.join()
+
+// console.log(Num)
+// console.log(newArr)
+
+
+let nums =[1,2,3,4,5,6]
+
+console.log("A:",nums)
+
+console.log("Result:",nums.slice(0,3))
+
+//console.log("Result:",)
+console.log("A:",nums)
+console.log("Result1",nums.splice(0,3))
