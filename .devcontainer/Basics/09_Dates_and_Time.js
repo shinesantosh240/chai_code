e7e5b119e62e2057f4eedc373a01c544d5dd3c0e
shinesantosh240 <shinesantosh240@gmail.com>
2025-02-19 02:37:03 +0000

    09_Dates_and_Time

---
+++ .devcontainer/Basics/09_Dates_and_Time.js
@@ -0,0 +1,39 @@
+// let d = new Date();
+
+// console.log(d)
+
+// console.log(d.toString())
+
+// console.log(typeof(d))
+
+// console.log(d.toDateString())
+
+// console.log(d.toISOString())
+
+// console.log(d.toLocaleDateString())
+
+// console.log(d.toUTCString())
+
+//let myDate = new Date(2025,0,23)
+
+// console.log(myDate.toDateString())
+
+// console.log(myDate.toLocaleDateString())
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp)
+
+let newDate = new Date(2025,5,22)
+console.log(newDate);
+
+console.log(newDate.getDay());
+
+console.log(newDate.getMonth())
+
+
+
+//console.log(getDayName(newDate))
+
+ 
+
+
