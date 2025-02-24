783520c7f607e9856b4a7638f2f32a96a6cba7fe
shinesantosh240 <shinesantosh240@gmail.com>
2025-02-24 02:03:37 +0000

    IIFE

---
+++ .devcontainer/scope/IIFE.js
@@ -0,0 +1,20 @@
+/****************Unnammed iffi******************************/
+ function chai(){
+    let Fname="Superman";
+    console.log(`my name is ${Fname}`)
+}
+chai();
+
+
+(function coffee(){
+    let Fname="WaterMan";
+    console.log(`my name is ${Fname}`)
+})();
+/****************Unnammed iffi******************************/
+(()=>{console.log("Hello Hites Chowdury")})();
+
+
+/****************Parameterised iffi******************************/
+
+( (num1)=>{console.log(num1);})("Ravish")
+
