58ccaa95eca26a5569e06e7cdfd0d1e121648db3
shinesantosh240 <shinesantosh240@gmail.com>
2025-02-26 01:13:29 +0000

    do

---
+++ .devcontainer/iterations/do.js
@@ -0,0 +1,14 @@
+let index=0;
+
+while(index <= 10){
+    console.log(`The value:${index}`);
+    index =index+1
+}
+
+let myArray =["Batman","Superman","Spiderman","Ironman","Heman"]
+let arr=0;
+
+while(arr<=myArray.length){
+    console.log(`The value of array : ${myArray[arr]}`);
+    arr = arr+1;
+}
