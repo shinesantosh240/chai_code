7f7191e6566f7e9100f72f9a390465040d35b241
shinesantosh240 <shinesantosh240@gmail.com>
2025-02-26 02:37:46 +0000

    five

---
+++ .devcontainer/iterations/five.js
@@ -0,0 +1,22 @@
+
+const coding =["Js","ruby","Java","Node","C++"]
+
+// coding.forEach( function(val){
+//     console.log(val);
+// })
+
+function printMe(item) {
+console.log(item);
+}
+//coding.forEach(printMe)
+
+const mycoding =[
+    { languagename :"Javascript",fileName:"Js"},
+    { languagename :"CPP",fileName:"C++"},
+    { languagename :"Python",fileName:"Py"}
+]
+
+mycoding.forEach((item)=>{
+console.log(item.languagename)
+} )
+
