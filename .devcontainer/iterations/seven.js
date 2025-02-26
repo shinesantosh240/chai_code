531a37caa4463ff142a7ac2c506febc808cd4a3d
shinesantosh240 <shinesantosh240@gmail.com>
2025-02-26 17:23:09 +0000

    seven

---
+++ .devcontainer/iterations/seven.js
@@ -0,0 +1,14 @@
+const myNums =[1,2,3,4,5,6,7,8,9,10]
+
+ //const newNums = myNums.map((num)=>{return num+10})
+
+ //console.log(newNums);
+
+ const numsA =[11,12,13,14,15,16,17,18,4,2,1]
+
+   const numsB = numsA
+   
+   .map(item => item+10)
+   .map(item=>item+1)
+   console.log(numsA)
+   console.log(numsB)
