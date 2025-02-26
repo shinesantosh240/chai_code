7249dd3c48ee088f5c9c63bd145b26d929ba9bca
shinesantosh240 <shinesantosh240@gmail.com>
2025-02-26 02:15:10 +0000

    four

---
+++ .devcontainer/iterations/four.js
@@ -0,0 +1,16 @@
+const myObject ={ js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+
+for (const key in myObject) {
+   //console.log(`${key} shortcut for: ${myObject[key]}`);
+        
+    }
+
+    const program =["js","C++","Shell","ruby","python"]
+
+    for (const key in program) {
+        console.log(program.key);
+    }
