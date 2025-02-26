262a3469970b8062817b51ed83d5068d35f853a7
shinesantosh240 <shinesantosh240@gmail.com>
2025-02-26 01:13:44 +0000

    forone

---
+++ .devcontainer/iterations/forone.js
@@ -0,0 +1,52 @@
+for (let index =0; index<10; index++){
+    const element =index;
+    if (element==5){
+        //console.log("5 is best number:",element)
+    }
+    //console.log(index)
+}
+
+for (let i=0;i<=10;i++)
+{
+//console.log(` i: ${i}`)
+for (let j=0;j<=10;j++)
+{
+    //console.log(`${j}`)
+    //console.log(i+'*'+j+ '='+i*+j)
+}
+}
+
+let myArray =["Varchar","Charvar","GPT"];
+console.log(myArray.length)
+
+for (let index = 0; index < myArray.length;index++) {
+    const element = myArray[index];
+    //console.log(element)
+    
+}
+/**********************continue used --> it will print all iteration */
+for (let index = 0; index < 20; index++) {
+    if(index==5)
+    {
+       console.log("5 is detected");
+       continue
+      break 
+      
+    }
+    console.log(index)
+    
+}
+
+/*********************************Break***************************************/
+
+for (let index = 0; index < 20; index++) {
+    if(index==5)
+    {
+       console.log("5 is detected");
+    
+      break 
+      
+    }
+    console.log(index)
+    
+}
