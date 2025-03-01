d333cea9ef257d79317d48a145cf2f6b1768c524
shinesantosh240 <shinesantosh240@gmail.com>
2025-03-01 17:36:08 +0000

    eight

--- .devcontainer/iterations/eight.js
+++ .devcontainer/iterations/eight.js
@@ -1,11 +1,11 @@
 const nums =[1,2,3,4,5]
 
-const sum = nums.reduce((accumulator,currentvalue)=>accumulator+currentvalue,0);
+const sum = nums.reduce((accumulator,currentvalue)=>accumulator+currentvalue,3);
 
 console.log(sum);
 
 const numbers = [1, 2, 3, 4];
-const result = numbers.reduce((acclator, cutValue) => acclator + cutValue, 0);
+const result = numbers.reduce((acclator, cutValue) => acclator + cutValue, 2);
 //console.log(acclator);
 console.log(result);  // Output: 10
 
@@ -14,7 +14,7 @@ const numsL =[1,2,3,4,5,6]
 const sumC = numsL.reduce(function(acc,currval){
     console.log(`accumulator ${acc} and currvsal ${currval}`)
     return acc+currval
-},0)
+},4)
 
 console.log(sumC);
 
